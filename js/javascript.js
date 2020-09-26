7d5e818c483ae9b14b0270633ad0f1fb8eed77a3
Ivan Krisko <vanya9.9@mail.ru>
2020-09-26 21:59:16 +1000

    fixed path to files

--- js/javascript.js
+++ js/javascript.js
@@ -36,13 +36,13 @@ const initialCards = [
         cardLink: "https://ski2.ru/statics/upload/curort/576/5870f57389e8d.jpg"},
     { 
         cardTitle:"Триозёрье",
-        cardLink: "/images/lakes.jpg" },
+        cardLink: "images/lakes.jpg" },
     { 
         cardTitle:"Ливадия",
-        cardLink: "/images/livadia.jpg" },
+        cardLink: "images/livadia.jpg" },
     { 
         cardTitle:"Вторая речка",
-        cardLink: "/images/home.jpg"
+        cardLink: "images/home.jpg"
     }
 ];
 
